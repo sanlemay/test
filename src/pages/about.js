09f14a070ef45c99a9c8288b8de526114ac96df7
sanlemay <sandrarlemay@gmail.com>
2018-10-23 18:30:10 -0400

    header-about addition

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Header from "../components/header"
+
+
+
+export default () => (
+    <div style ={{ color: 'teal'}}>
+    <Header headerText="About Gatsby" />
+
+    <p>Such wow. Very React.</p>
+   
+    </div>
+) 
